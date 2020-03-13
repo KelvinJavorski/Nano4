db18bc6552550cdfecabf14ab1e8aa3ba0f1f7db
Saulo <oi@saulo.tech>
2020-03-13 14:16:47 -0300

    Adiciona modelo de ritmo

--- GameBusiness/GameBusiness/Model/Model.swift
+++ GameBusiness/GameBusiness/Model/Model.swift
@@ -13,12 +13,32 @@ class Model {
     static let shared = Model()
     
     var phases: [Phase] = [
-        Phase(music: "", steps:
+        Phase(music: "Try.mp3", steps:
         [
-            Step(CGPoint(x: 0.0,  y: -10.0), TimeInterval(0.5)),
-            Step(CGPoint(x: 10.0, y: -10.0), TimeInterval(0.5)),
-            Step(CGPoint(x: 10.0, y: 0.0  ), TimeInterval(0.5)),
-            Step(CGPoint(x: 10.0, y: 10.0 ), TimeInterval(0.5))
+            // Eu usei as coordenadas como porcentagens [-100, 100]
+            Step(CGPoint(x: 50.0, y: 70.0), TimeInterval(0.0)),
+            Step(CGPoint(x: 60.0, y: 20.0), TimeInterval(2.09)),
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.09)),//4.18
+            Step(CGPoint(x: 10.0, y: 10.0), TimeInterval(2.85)),//7.03
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.19)),//9.22
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(1.97)),//11.19
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.86)),//14.05
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval()),//15.00
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(),//16.13
+        ]
+        )
+        Phase(music: "Stranger_Danger.mp3", steps:
+        [
+            // Eu usei as coordenadas como porcentagens [-100, 100]
+            Step(CGPoint(x: 50.0, y: 70.0), TimeInterval(0.0)),
+            Step(CGPoint(x: 60.0, y: 20.0), TimeInterval(1.94)),
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.09)),//4.18
+            Step(CGPoint(x: 10.0, y: 10.0), TimeInterval(2.85)),//7.03
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.19)),//9.22
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(1.97)),//11.19
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.86)),//14.05
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval()),//15.00
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(),//16.13
         ]
         )
     ]
