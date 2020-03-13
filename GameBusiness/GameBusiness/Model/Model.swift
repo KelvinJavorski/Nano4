e9b8f911adc8e272c8640f97d449a2d7e043b6bf
Saulo <oi@saulo.tech>
2020-03-13 15:04:53 -0300

    Auto stash before merge of "develop" and "origin/develop"
    Adiciona arquivo de música

--- GameBusiness/GameBusiness/Model/Model.swift
+++ GameBusiness/GameBusiness/Model/Model.swift
@@ -27,7 +27,7 @@ class Model {
 //            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval()//16.13
         ]
         ),
-        Phase(music: "Stranger_Danger.mp3", steps:
+        Phase(music: "Neon_Storm.mp3", steps:
         [
             // Eu usei as coordenadas como porcentagens [-100, 100]
             Step(CGPoint(x: 50.0, y: 70.0), TimeInterval(0.0)),
@@ -36,7 +36,9 @@ class Model {
             Step(CGPoint(x: 10.0, y: 10.0), TimeInterval(2.85)),//7.03
             Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.19)),//9.22
             Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(1.97)),//11.19
-            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.86))//,//14.05
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.86)),//14.05
+            Step(TimeInterval(2.0)),
+            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval(2.0000)),
 //            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval()),//15.00
 //            Step(CGPoint(x: 10.0, y: 0.0 ), TimeInterval()//16.13
         ]
